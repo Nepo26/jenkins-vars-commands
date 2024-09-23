8ad964ae1e2f6d42d298fec2c7f2f2c8531ace41
GitHub Action <action@github.com>
2024-09-23 19:58:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/nepo26/jenkinsvarscommands/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/nepo26/jenkinsvarscommands/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nepo26.jenkinsvarscommands.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
